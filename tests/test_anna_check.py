f769db262d5cc3f5fb8adfeaccfa8a1a0ded6d10
kesaeva2006-commits <kesaeva2006@gmail.com>
2026-05-12 01:04:07 +0300

    Delete tests/test_anna_check.py

--- tests/test_anna_check.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Тест 1: проверяет, что в ветке Ани есть файл atm.py
-def test_atm_file_exists_in_anna_branch():
-    assert os.path.exists('atm.py'), "Файл atm.py не найден (должен быть в ветке Ани)"
-
-# Тест 2: проверяет, что в ветке Ани есть файл route_utils.py
-def test_route_utils_exists_in_anna_branch():
-    assert os.path.exists('route_utils.py'), "Файл route_utils.py не найден (должен быть в ветке Ани)"
