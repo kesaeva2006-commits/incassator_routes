59145fef11205acb32fc474ce504761d2a81752d
luiza-khachatrian <luizakhachatrian@gmail.com>
2026-06-03 02:42:57 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -35,10 +35,10 @@ def generate_atms(count: int = 10, seed: int = 42) -> list[Atm]:
         cap_in = random.randint(8000, 30000)   # приём денег
         cap_out = random.randint(10000, 25000)  # выдача денег
 
-        mean_in = random.uniform(100, 180)
-        std_in = random.uniform(10, 35)
-        mean_out = random.uniform(90, 160)
-        std_out = random.uniform(10, 30)
+        mean_in = random.uniform(80, 130)
+        std_in = random.uniform(8, 25)
+        mean_out = random.uniform(70, 110)
+        std_out = random.uniform(8, 20)
 
         # Создаём объект банкомата
         atm = Atm(
